a97830c794f3acb8156f5b722ccf9de499ff3058
Mikhail Kudriavtsev <mikhail.kudriavtsev2@mail.dcu.ie>
2023-10-22 17:46:02 +0100

    ready for work

--- sharp.py
+++ sharp.py
@@ -1,6 +1,7 @@
 from PIL import Image
+import sys
 
-def apply_sharpen_effect(input_image_path, output_image_path):
+def apply_sharpen_filter(input_image_path, output_image_path):
     try:
         # Open the image from the file
         image = Image.open(input_image_path)
@@ -9,12 +10,13 @@ def apply_sharpen_effect(input_image_path, output_image_path):
         # Create an empty image for the result
         sharpened_image = Image.new('RGB', (width, height))
 
-        # Sharpening filter kernel
-        kernel = [[-1, -1, -1],
-                  [-1, 9, -1],
-                  [-1, -1, -1]]
+        # Sharpening filter kernel (3x3 matrix)
+        kernel = [
+            [0, -1, 0],
+            [-1, 5, -1],
+            [0, -1, 0]
+        ]
 
-        # Apply the sharpening effect
         for x in range(1, width - 1):
             for y in range(1, height - 1):
                 new_pixel = [0, 0, 0]
@@ -31,16 +33,16 @@ def apply_sharpen_effect(input_image_path, output_image_path):
 
         # Save the processed image
         sharpened_image.save(output_image_path)
-        print(f"Sharpening effect applied and saved to {output_image_path}")
+        print(f"Sharpening filter applied and saved to {output_image_path}")
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python sharpen_effect.py <input_image> <output_image>")
+        print("Usage: python sharpen_filter.py <input_image> <output_image>")
         sys.exit(1)
 
     input_image_path = sys.argv[1]
     output_image_path = sys.argv[2]
-    apply_sharpen_effect(input_image_path, output_image_path)
+    apply_sharpen_filter(input_image_path, output_image_path)
